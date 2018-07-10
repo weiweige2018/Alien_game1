cf67d16db70af1dd96c63f896bc12ece35f2f065
weiweige2018 <15902061062@139.com>
2018-07-10 23:52:56 +0800

    0.1

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,12 +6,14 @@ def run_game():
     screen = pygame.display.set_mode((1200,800))
     pygame.display.set_caption("Alien Invasion")
 
+    bg_color = (230,230,230)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-                
 
+        screen.fill(bg_color)
         pygame.display.flip()
 
 run_game()
